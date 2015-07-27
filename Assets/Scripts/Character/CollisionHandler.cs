101f9de5f43eb21f04412e65ab3a08693735608a
Yiou Chen <chen.yiou1992@gmail.com>
2015-07-27 00:21:21 -0400

    TODO : hero goes into wall when entering from bottom left

--- Assets/Scripts/Character/CollisionHandler.cs
+++ Assets/Scripts/Character/CollisionHandler.cs
@@ -6,9 +6,9 @@ using System;
 public class CollisionHandler : MonoBehaviour {
     Speed speed;
     [SerializeField]
-    private float margin = 0.1f;
-    int horizontalRays = 4;
-    int verticalRays = 4;
+    private float margin = 0.2f;
+    int horizontalRays = 8;
+    int verticalRays = 8;
 
     void Awake()
     {
@@ -38,44 +38,61 @@ public class CollisionHandler : MonoBehaviour {
         Bounds bounds = GetComponent<Collider2D>().bounds;
         Vector2 center = bounds.center;
         Vector2 extents = bounds.extents;
+        Vector2 horizontalHalf=new Vector2(bounds.extents.x * (1 - margin), 0);
+        Vector2 verticalHalf=new Vector2(0,bounds.extents.y * (1 - margin));
+        //Vector2 left = center - horizontalHalf;
+       // Vector2 right=center+
         speed.Delta = speed.currentVel * deltaTime;
         float y = speed.Delta.y;
         float x = speed.Delta.x;
         RaycastHit2D hitX, hitY;
-        if (y > 0)
+        for (int i = 0; i < verticalRays; i++)
         {
+            float lerpAmount = (float)i / (float)(verticalRays - 1);
+            Vector2 origin = Vector2.Lerp(center - horizontalHalf, center + horizontalHalf, lerpAmount);
             
-            hitY=Raycast(center, Vector2.up, extents.y+y);
-            
+            if (y > 0)
+            {
+
+                hitY = Raycast(origin, Vector2.up, extents.y + y);
+
+            }
+            else
+            {
+
+                hitY = Raycast(origin, Vector2.down, extents.y - y);
+
+            }
+            if (hitY.collider != null)
+            {
+                //TODO change this so that y is equal to the closest
+                y = extents.y - hitY.distance;
+            }
         }
-        else
+        for (int i = 0; i < horizontalRays; i++)
         {
-            
-            hitY=Raycast(center, Vector2.down,extents.y-y);
-           
-        }
-        if (x > 0)
-        {
-            
-            hitX=Raycast(center, Vector2.right, extents.x+x);
-            
-        }
-        else
-        {
-            hitX=Raycast(center, Vector2.left, extents.x-x);
-            
+            float lerpAmount = (float)i / (float)(verticalRays - 1);
+            Vector2 origin = Vector2.Lerp(center - verticalHalf, center + verticalHalf, lerpAmount);
+            if (x > 0)
+            {
+
+                hitX = Raycast(origin, Vector2.right, extents.x + x);
+
+            }
+            else
+            {
+                hitX = Raycast(origin, Vector2.left, extents.x - x);
+
+            }
+            if (hitX.collider != null)
+            {
+                //TODO change this so that y is equal to the closest
+                x = extents.x - hitX.distance;
+            }
+        
         }
         
-        int mask = LayerMask.NameToLayer("blocking");
-        if (hitX.collider != null)
-        {
-
-            x = extents.x - hitX.distance;
-        }
-        if (hitY.collider!=null){
-
-            y = extents.y - hitY.distance;
-        }
+      
         
         return new Vector2(x,y);
 
